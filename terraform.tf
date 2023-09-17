cbc43b2af2502a6c71675fcce0841e87f5ddca2c
Darren Reddick <darren.reddick@ecs.co.uk>
2023-09-17 11:02:54 +0100

    k

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,6 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=3.63.0"
-    }
+    var.terraform_config.providers
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -13,3 +13,15 @@ variable "single_nat_gateway" {
   description = "Only create a single NAT gateway - useful option for testing"
   default     = true
 }
+
+variable "terraform_config" {
+  type = map
+  default = {
+    providers = { 
+      aws = {
+        source  = "hashicorp/aws"
+        version = ">=3.63.0"
+      }
+    }
+  }
+}
